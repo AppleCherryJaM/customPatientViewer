454e32a1eb0332ca91d93de3b1f02db48aa74d71
Jam <jamax.cherry@gmail.com>
2022-07-11 10:42:41 +0300

    Deleted comments

--- force-app/main/default/aura/DatatableComponent/DatatableComponentController.js
+++ force-app/main/default/aura/DatatableComponent/DatatableComponentController.js
@@ -9,33 +9,6 @@
         helper.sortData(cmp, fieldName, sortDirection);
     },
     loadMoreData: function(cmp, event, helper) {
-        //     try {
-        //         // cmp.set('v.rowsToLoad', 3);
-
-        //         event.getSource().set("v.isLoading", true);
-        //         // cmp.set('v.loadMoreStatus', 'Loading');
-
-        //         // promiseData = helper.fetchData(cmp, fetchData, rowsToLoad);
-        //         var data = cmp.get("v.data");
-        //         // promiseData.then($A.getCallback(function(data) {
-        //         if (cmp.get('v.patientList').length === cmp.get('v.data').length) {
-        //             cmp.set('v.enableInfiniteLoading', false);
-        //             cmp.set('v.loadMoreStatus', 'No more data to load');
-        //             // console.log('ZALUPA!', JSON.parse(JSON.stringify(cmp.get("v.data"))));
-        //         } else {
-        //             var currentData = cmp.get('v.data2');
-        //             var newData = currentData.concat(data);
-        //             cmp.set('v.data', newData);
-        //             cmp.set('v.loadMoreStatus', '');
-        //             // }
-        //             event.getSource().set("v.isLoading", false);
-        //             // }));
-        //             console.log('SEMEN!', JSON.parse(JSON.stringify(cmp.get("v.data"))));
-        //         }
-        //     } catch (err) {
-        //         console.log(err);
-        //     }
-        console.log('TARGET!');
         try {
             var cmpEvent = $A.get("e.c:PatientDatatableLoadEvent");
             cmpEvent.setParams({ "isLoad": true });
@@ -43,10 +16,5 @@
         } catch (error) {
             console.log("ERROR: ", error.message);
         }
-    },
-
-    testClick: function(cmp) {
-        console.log("START!");
-
     }
 });
--- force-app/main/default/aura/PatientVewerComponent/PatientVewerComponentController.js
+++ force-app/main/default/aura/PatientVewerComponent/PatientVewerComponentController.js
@@ -1,38 +1,29 @@
 ({
     onInit: function(cmp, event, helper) {
-        var columns = [{
-                label: 'First Name',
-                fieldName: 'linkName',
-                type: 'url',
-                typeAttributes: {
-                    label: { fieldName: 'firstName' },
-                    target: '_blank'
-                },
-                cellAttributes: {
-                    class: 'slds-box',
-                    alignment: 'center'
-                }
-            },
-            {
-                label: 'Last Name',
-                fieldName: 'lastName',
-                typeAttributes: {
-                    label: { fieldName: 'lastName' },
-                    target: '_blank'
-                },
-                cellAttributes: {
-                    class: 'slds-box',
-                    alignment: 'center'
-                }
-            }
-        ];
-        cmp.set("v.columns", columns);
+        helper.getColumns(cmp);
         helper.getFacilities(cmp);
+
     },
 
     handleComponentEvent: function(cmp, event, helper) {
         var value = event.getParam("message");
         helper.getPatients(cmp, value);
         cmp.set("v.isComboboxSelected", true);
+    },
+    addNewInfo: function(cmp, event, helper) {
+        var value = event.getParam("isLoad");
+        console.log("addNewInfo is entered!");
+
+        var storage = cmp.get("v.storage");
+        var data = cmp.get("v.data");
+
+        if (storage.length === 0) {
+            cmp.set("v.isLoad", false);
+        } else {
+            data.push(storage[0]);
+            cmp.set("v.data", data);
+            storage.shift();
+            cmp.set("v.storage", storage);
+        }
     }
 })
